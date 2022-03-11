7d6ca3b8b9c31e59cdd94639acf940d154b1fd48
A80167811050629\alum-01 <adriacostasalse@gmail.com>
2022-03-11 17:51:13 +0100

    test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000
 var pepe = ["hola", "adios"];
 
 app.get('/', function (req, res) {
-  res.send("hola como estamos");
+  res.json(pepe)
 });
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
