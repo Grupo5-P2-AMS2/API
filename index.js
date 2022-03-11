ef6f11b131a7f6bbe2252c30507138b185e70a0f
A80167811050629\alum-01 <adriacostasalse@gmail.com>
2022-03-11 17:02:45 +0100

    test

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const app = express();
 
 var pepe = ["hola", "adios"];
 
 app.get('/', function (req, res) {
-  res.json(pepe);
+  res.send("hola como estamos");
 });
