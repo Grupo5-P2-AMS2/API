054e1f0dd580ec2fb7bd881b4baf1019a81339eb
victormerch <1Victormerch@gmail.com>
2022-03-30 16:31:21 +0200

    cleanning code 13

--- server.js
+++ server.js
@@ -194,7 +194,7 @@ app.use(function(req, res, next) {
         boolean = true;
         var arrayUser = await UserModel.find({session_token:req.query.session_token});
         var arrayVRtaskID = await CourseModel.find({"vr_tasks.ID":req.query.VRtaskID});
-        if(arrayUser[0].ID != undefined && req.query.session_token != undefined){
+        if(arrayUser[0] != undefined && req.query.session_token != undefined){
           if(arrayVRtaskID != [] && req.query.VRtaskID != undefined){ 
             PinsModel.insertMany({"pin":pin,"userId":arrayUser[0].ID,"VRtaskID":req.query.VRtaskID});
             res.json({"status":"OK","PIN":pin})
