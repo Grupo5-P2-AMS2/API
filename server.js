3512dfb28844ce60da67e7ae23228bfd3fa772f9
victormerch <1Victormerch@gmail.com>
2022-04-01 20:02:40 +0200

    cors 2

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose.connect('mongodb+srv://victor:WzRZK8JRGBo8dyML@cluster0.vudsg.mongodb.n
   //res.header('Access-Control-Allow-Methods', '*');
   //res.header('Allow', '*');
   //next();
-app.use(bodyParser.urlencoded({ extended: false }),(req, res, next) => {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
