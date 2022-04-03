d834bbe199c09501b601252f5c6e6dda04c26ba6
victormerch <1Victormerch@gmail.com>
2022-04-03 20:15:50 +0200

    metodo POST arreglado 2

--- server.js
+++ server.js
@@ -24,14 +24,14 @@ mongoose.connect('mongodb+srv://victor:WzRZK8JRGBo8dyML@cluster0.vudsg.mongodb.n
   //res.header('Access-Control-Allow-Methods', '*');
   //res.header('Allow', '*');
   //next();
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
   
-// });
+});
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
   //=====GET====
